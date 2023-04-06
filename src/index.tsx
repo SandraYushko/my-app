16c641d15070d7484a767ee0d6556d9e153f3766
SandraYushko <shyria6@gmail.com>
2023-04-06 23:31:17 +0300

    1 06/04/2023
    start

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,25 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div> Hello </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+let a = {
+  name: "Sasha",
+  age: 10,
+  address: {
+    city: "Minsk",
+    country: "Belarus"
+  }
+};
+
+
+console.log(a);
