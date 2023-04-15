3043e5de73175bd2e2820f3966fc64681a072212
SandraYushko <shyria6@gmail.com>
2023-04-15 23:52:10 +0300

    Domashnee zadanie (React TS - Kabzda kak podrobno - lesson 04)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./components/Accordion/Accordion";
+import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
 
 
 function hello() {
     //debugger
-
     alert("Hello IT-KAMASUTRA")
 }
-
 hello();
 
 function App() {
@@ -23,8 +21,8 @@ function App() {
             <Rating value={1} />
             <Rating value={2} />
             <Rating value={3} />
-            <Accordion titleValue={"Menu"} />
-            <Accordion titleValue={"Users"} />
+            <Accordion titleValue={"Menu"} collapsed={true} />
+            <Accordion titleValue={"Users"} collapsed={false} />
             Article 2
             <Rating value={4} />
             <Rating value={5} />
@@ -36,7 +34,6 @@ type PageTitlePropsType = {
     title: string
 }
 function PageTitle(props: PageTitlePropsType) {
-    debugger
     console.log("PageTitle rendering")
     return <h1> {props.title} </h1>
 }
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,34 +2,39 @@ import React from "react";
 
 type AccordionPropsType = {
     titleValue: string
+    collapsed: true|false
 }
 
-function Accordion(props: AccordionPropsType) {
+export function Accordion(props: AccordionPropsType) {
     console.log("Accordion rendering")
-    return <div>
-        <AccordionTitle title={props.titleValue}/>
-        <AccordionBody />
-    </div>
+    if (props.collapsed === true) {
+        return <AccordionTitle title={props.titleValue}/>
+    } else {
+        return <div>
+            <AccordionTitle title={props.titleValue}/>
+            <AccordionBody/>
+        </div>
+    }
+
+    type AccordionTitlePropsType = {
+        title: string
+    }
+    function AccordionTitle(props: AccordionTitlePropsType) {
+        console.log("AccordionTitle rendering")
+        return <h3>---{props.title}---</h3>
+    }
 }
 
-type AccordionTitlePropsType = {
-    title: string
+type AccordionBodyPropsType = {
 }
-
-function AccordionTitle(props: AccordionTitlePropsType) {
-    console.log("AccordionTitle rendering")
-    return <h3>---{props.title}---</h3>
-
-}
-
-function AccordionBody() {
-    console.log("AccordionBody rendering")
-    return <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>
-}
+    function AccordionBody(props: AccordionBodyPropsType) {
+        console.log("AccordionBody rendering")
+        return <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    }
 
 
-export default Accordion;
+    //export default Accordion;
