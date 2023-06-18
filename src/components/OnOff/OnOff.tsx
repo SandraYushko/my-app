1219ed19eab65a1b4599f929233391368d65e405
SandraYushko <shyria6@gmail.com>
2023-06-18 18:09:14 +0300

    (React TS - Kabzda kak podrobno - lesson 06) useState

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -6,18 +6,39 @@ type OnOffPropsType = {
 }
 
 export function OnOff(props: OnOffPropsType) {
+    const onStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: props.value ? "green" : "white"
+    };
+
+    const offStyle = {
+        width: "30px",
+        height: "20px",
+        border: "1px solid black",
+        display: 'inline-block',
+        marginLeft: "2px",
+        padding: '2px',
+        backgroundColor: props.value ? "white" : "red"
+    };
+
+    const indicatorStyle = {
+        width: "10px",
+        height: "10px",
+        borderRadius: "5px",
+        border: "1px solid black",
+        display: 'inline-block',
+        marginLeft: "5px",
+        backgroundColor: props.value ? "green" : "red"
+    };
 
         return <div>
-            {props.value && <span>
-                <button className = {s.on}>{'On'}</button>
-                <button>{'Off'}</button>
-                <input type={'radio'} className={s.on}/>
-            </span>}
-            {!props.value && <span>
-                <button>{'On'}</button>
-                <button className = {s.off}>{'Off'}</button>
-                <input type={'radio'} className={s.off}/>
-            </span>}
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={indicatorStyle}></div>
         </div>
 }
 
