8fa6f394ac58ef97368153b70630aaf6398032d6
SandraYushko <shyria6@gmail.com>
2023-11-25 17:04:40 +0300

    (React TS - Kabzda kak podrobno - lesson 10-2.) storybook Accordion

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,8 +1,6 @@
-//import type {Meta, StoryObj} from '@storybook/react';
 import {Accordion} from './Accordion';
 import React, {useState} from 'react';
 import {action} from '@storybook/addon-actions';
-
 
 export default {
     component: Accordion
