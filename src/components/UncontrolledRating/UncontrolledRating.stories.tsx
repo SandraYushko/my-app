9af3c09a2a8d1600d1df9261c67c6dee77495366
SandraYushko <shyria6@gmail.com>
2023-11-25 17:33:26 +0300

    (React TS - Kabzda kak podrobno - lesson 10-2.) storybook UncontrolledRating

---
+++ src/components/UncontrolledRating/UncontrolledRating.stories.tsx
@@ -0,0 +1,11 @@
+import {UncontrolledRating} from './UncontrolledRating';
+import React, {useState} from 'react';
+import {action} from '@storybook/addon-actions';
+
+export default {
+    component: UncontrolledRating
+};
+
+export const UncontrRating = () => {
+    return <UncontrolledRating/>
+}
