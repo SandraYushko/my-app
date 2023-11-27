73a9e5cde096a575d5534ce04224c4a572ba66a6
SandraYushko <shyria6@gmail.com>
2023-11-27 23:35:04 +0300

    (React TS - Kabzda kak podrobno - lesson 10-2.) storybook UncontrolledRating Dimich

--- src/components/UncontrolledRating/UncontrolledRating.stories.tsx
+++ src/components/UncontrolledRating/UncontrolledRating.stories.tsx
@@ -1,11 +1,27 @@
 import {UncontrolledRating} from './UncontrolledRating';
-import React, {useState} from 'react';
+import React from 'react';
+import {RatingValuePropsType} from '../Rating/Rating';
 import {action} from '@storybook/addon-actions';
 
 export default {
-    component: UncontrolledRating
+    component: UncontrolledRating,
 };
-
-export const UncontrRating = () => {
-    return <UncontrolledRating/>
+ const callback = action('onChange')
+export const UncontrolledRating0 = () => {
+    return <UncontrolledRating defaultValue={0} onChange={callback}/>
+}
+export const UncontrolledRating1 = () => {
+    return <UncontrolledRating defaultValue={1} onChange={callback}/>
+}
+export const UncontrolledRating2 = () => {
+    return <UncontrolledRating defaultValue={2} onChange={callback}/>
+}
+export const UncontrolledRating3 = () => {
+    return <UncontrolledRating defaultValue={3} onChange={callback}/>
+}
+export const UncontrolledRating4 = () => {
+    return <UncontrolledRating defaultValue={4} onChange={callback}/>
+}
+export const UncontrolledRating5 = () => {
+    return <UncontrolledRating defaultValue={5} onChange={callback}/>
 }
